623558e8c8bccf40056e20104d35e4840cc84e10
edoardo <edolops.el@gmail.com>
2024-11-29 10:49:45 +0100

    aggiunto react

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const titolo = document.getElementsByTagName("h1");
-console.log(titolo[0]);
-const lista = document.querySelectorAll("li");
-console.log(lista);
-
-// [li, li, li, li]
-// const listanuova = [...lista]
-const listafiltrata = Array.from(lista).filter((x, i) => i % 2 === 0);
-console.log(listafiltrata);
